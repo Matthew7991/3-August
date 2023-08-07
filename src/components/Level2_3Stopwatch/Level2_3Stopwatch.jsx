03e6bc43832af473ea8c8eec2fcf0590867084d1
Matthew7991 <Matthew7991@gmx.de>
2023-08-07 15:10:55 +0200

    testing stopwatch

--- src/components/Level2_3Stopwatch/Level2_3Stopwatch.jsx
+++ src/components/Level2_3Stopwatch/Level2_3Stopwatch.jsx
@@ -1,54 +1,62 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function Level2_3Stopwatch() {
-  // i give up doing it without useEffect
-  const stopWatchDefaultDisplay = "00:00:00"
-  
-  const [stopWatchActivated, setStopWatchActivated] = useState(false)
-  const [stopWatchRunning, setStopWatchRunning] = useState(false)
-  const [stopWatchTime, setStopWatchTime] = useState(stopWatchDefaultDisplay)
-  
-  const stopWatchStartHandler = () => {
-    setStopWatchActivated(true)
-    setStopWatchRunning(true)
+	// i give up doing it without useEffect
+	const stopWatchDefaultDisplay = '00:00:00';
 
-    setInterval(stopWatch, 10);
-  }
-  const stopWatchStopHandler = () => {
-    clearInterval(stopWatch)
+	const [stopWatchActivated, setStopWatchActivated] = useState(false);
+	const [stopWatchRunning, setStopWatchRunning] = useState(false);
+	const [stopWatchTime, setStopWatchTime] = useState(stopWatchDefaultDisplay);
+	const [time, setTime] = useState(0);
 
-    setStopWatchRunning(false)
-  }
+	let intervalID;
 
-  const stopWatchReset = () => {
-    setStopWatchActivated(false)
-    setStopWatchRunning(false)
-    setStopWatchTime(stopWatchDefaultDisplay)
-  }
+	const stopWatchStartHandler = () => {
+		setStopWatchActivated(true);
+		setStopWatchRunning(true);
+	};
 
-  const [time, setTime] = useState(0)
-  
-  const stopWatch = () => {
+	const stopWatchStopHandler = () => {
+		setStopWatchRunning(false);
+	};
 
-    const milliseconds = (time % 100).toString().padStart(2, "0")
-    const seconds = Math.floor(((time % 6000) / 100)).toString().padStart(2, "0")
-    const minutes = Math.floor(((time % 360000) / 100)).toString().padStart(2, "0")
+	const stopWatchReset = () => {
+		setStopWatchActivated(false);
+		setStopWatchRunning(false);
+		setStopWatchTime(stopWatchDefaultDisplay);
+	};
 
-    setStopWatchTime(`${minutes}:${seconds}:${milliseconds}`)
-    setTime(prevTime => prevTime + 1)
-  }
+	const stopWatch = () => {
+		setTime((prevTime) => prevTime + 1);
+	};
 
-  return (
-    <article>
-      <p>{stopWatchTime}</p>
-      {
-      stopWatchRunning 
-      ? <button onClick={stopWatchStopHandler}>Stop</button> 
-      : <button onClick={stopWatchStartHandler}>Start</button>
-      }
-      {stopWatchActivated && <button onClick={stopWatchReset}>Reset</button>}
-    </article>
-  )
+	const milliseconds = (time % 100).toString().padStart(2, '0');
+	const seconds = Math.floor((time % 100) / 60)
+		.toString()
+		.padStart(2, '0');
+	const minutes = Math.floor((time % 6000) / 60)
+		.toString()
+		.padStart(2, '0');
+
+	useEffect(() => {
+		if (stopWatchRunning) {
+			clearInterval(intervalID);
+			intervalID = setInterval(stopWatch, 10);
+		}
+		setStopWatchTime(`${minutes}:${seconds}:${milliseconds}`);
+	}, [time, stopWatchRunning]);
+
+	return (
+		<article>
+			<p>{stopWatchTime}</p>
+			{stopWatchRunning ? (
+				<button onClick={stopWatchStopHandler}>Stop</button>
+			) : (
+				<button onClick={stopWatchStartHandler}>Start</button>
+			)}
+			{stopWatchActivated && <button onClick={stopWatchReset}>Reset</button>}
+		</article>
+	);
 }
 
-export default Level2_3Stopwatch
+export default Level2_3Stopwatch;
