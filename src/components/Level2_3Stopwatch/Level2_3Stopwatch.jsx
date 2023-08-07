b6639a2a793e3059c589b5313a2b7ba757fa696c
Matthew7991 <Matthew7991@gmx.de>
2023-08-07 15:29:33 +0200

    stopwatch fixed

--- src/components/Level2_3Stopwatch/Level2_3Stopwatch.jsx
+++ src/components/Level2_3Stopwatch/Level2_3Stopwatch.jsx
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from 'react';
 
 function Level2_3Stopwatch() {
-	// i give up doing it without useEffect
 	const stopWatchDefaultDisplay = '00:00:00';
 
 	const [stopWatchActivated, setStopWatchActivated] = useState(false);
 	const [stopWatchRunning, setStopWatchRunning] = useState(false);
 	const [stopWatchTime, setStopWatchTime] = useState(stopWatchDefaultDisplay);
 	const [time, setTime] = useState(0);
-
-	let intervalID;
+	const [intervalId, setIntervalId] = useState(null);
 
 	const stopWatchStartHandler = () => {
 		setStopWatchActivated(true);
 		setStopWatchRunning(true);
+		setIntervalId(setInterval(stopWatch, 10));
 	};
 
 	const stopWatchStopHandler = () => {
 		setStopWatchRunning(false);
+		clearInterval(intervalId);
 	};
 
 	const stopWatchReset = () => {
 		setStopWatchActivated(false);
 		setStopWatchRunning(false);
-		setStopWatchTime(stopWatchDefaultDisplay);
+		setTime(0);
+		clearInterval(intervalId);
 	};
 
 	const stopWatch = () => {
@@ -31,20 +32,16 @@ function Level2_3Stopwatch() {
 	};
 
 	const milliseconds = (time % 100).toString().padStart(2, '0');
-	const seconds = Math.floor((time % 100) / 60)
+	const seconds = Math.floor((time / 100) % 60)
 		.toString()
 		.padStart(2, '0');
-	const minutes = Math.floor((time % 6000) / 60)
+	const minutes = Math.floor((time / 6000) % 60)
 		.toString()
 		.padStart(2, '0');
 
 	useEffect(() => {
-		if (stopWatchRunning) {
-			clearInterval(intervalID);
-			intervalID = setInterval(stopWatch, 10);
-		}
 		setStopWatchTime(`${minutes}:${seconds}:${milliseconds}`);
-	}, [time, stopWatchRunning]);
+	}, [time]);
 
 	return (
 		<article>
